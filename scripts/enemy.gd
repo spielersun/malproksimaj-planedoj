7c95e2d7c3b3e34f5829cafa64c186da44d09f3c
Güneş Eser <spielersun@gmail.com>
2018-03-29 23:01:41 +0300

    enemy upd1

---
+++ scripts/enemy.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+export var velocity = Vector2()
+
+func _ready():
+	set_process(true)
+	pass
+
+func _process(delta):
+	translate(velocity * delta)
+	
+	if position.x <= 0  
+		queue_free()
+	pass
