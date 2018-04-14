f160dd797850873f8b43d602521feb65f5dfa1ef
Güneş Eser <spielersun@gmail.com>
2018-04-14 23:02:07 +0300

    gameplay upd4

--- scripts/birdcamera.gd
+++ scripts/birdcamera.gd
@@ -1,4 +1,11 @@
 extends Camera2D
 
+onready var bird = utils._get_main_node().get_node("bird")
+
 func _ready():
+	set_physics_process(true)
+	pass
+
+func _physics_process(delta):
+	position = Vector2(bird.position.x, position.y)
 	pass
