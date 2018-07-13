2e9d67b52076f03cf598f483d24f4f250fd68088
Güneş Eser <spielersun@gmail.com>
2018-07-13 23:44:04 +0300

    a little puppy rolls in our backyard
    oyyhh/

--- scripts/fp-lorenna.gd
+++ scripts/fp-lorenna.gd
@@ -5,7 +5,7 @@ onready var spawner = get_tree().get_root().get_node("fp-test").find_node("fp-en
 export var speed = 100
 export var health = 5
 
-onready var ship_anims = $ship
+onready var vehicle = $vehicle
 onready var collision = $shape
 
 var dead = false 
@@ -14,10 +14,12 @@ func _process(delta):
 	move(delta)
 	
 	if position.x < -100:
+		spawner.lorenna_presence = false
 		queue_free()
 		
 func move(delta):
 	position.x -= speed * delta
+	vehicle.play("move-left")
 	
 func add_damage(damage):
 	health -= damage
