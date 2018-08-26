0ecee6067c76198ebeb2a78ccd46f111862d9106
Güneş Eser <spielersun@gmail.com>
2018-08-26 22:13:00 +0300

    donde estan mis ruedas
    ¡ja, ja, ja! ¿quien es el guason ahora?

--- scripts/astro_drop.gd
+++ scripts/astro_drop.gd
@@ -1,22 +1,50 @@
 extends Area2D
 
-export var speed = 200
+export(PackedScene) var explosion_big
+
+onready var object = $object
+
+export var speed = 250
 export var damage = 5
 
 var origin_pos
+var scatter_constant
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+	randomize()
+	scatter_constant = rand_range(-100, 100)
+	object.play("active")
+	connect("body_entered", self, "_on_body_entered")
 
 func _process(delta):
+	rotation += 0.1
+	
 	if origin_pos == "east":
 		position.x -= (speed * delta)
+		position.y += (scatter_constant * delta)
 	elif origin_pos == "west":
 		position.x += (speed * delta)
+		position.y -= (scatter_constant * delta)
+	elif origin_pos == "north":
+		position.y += (speed * delta)
+		position.x -= (scatter_constant * delta)
+	elif origin_pos == "south":
+		position.y -= (speed * delta)
+		position.x += (scatter_constant * delta)
 	
 	if position.x < -500:
 		queue_free()
-	elif position.x > 2000:
+	elif position.x > 2100:
+		queue_free()
+	
+	if position.y < -500:
+		queue_free()
+	elif position.y > 1400:
+		queue_free()
+
+func _on_body_entered(area):
+	if area.is_in_group("player"):
+		var new_explosion = explosion_big.instance()
+		new_explosion.position = position
+		get_parent().add_child(new_explosion)
 		queue_free()
--- scripts/astro_spawner.gd
+++ scripts/astro_spawner.gd
@@ -5,30 +5,47 @@ export(PackedScene) var astro_drop
 var east_x = 1800
 var east_y = 200
 
-var west_x= -200
+var west_x = -200
 var west_y = 500
 
-var drop_count = 5
+var north_x =  600
+var north_y = -200
 
-var origin_points = ["east","west"]
+var south_x = 700
+var south_y = 1100
+
+var drop_count = 50
+
+var spawn_x
+var spawn_y
+
+var origin_points = ["east","west","north","south"]
 
 func _ready():
 	for drop in drop_count:
 		astro_drop()
 		randomize()
-		yield(belt.create_timer(rand_range(1.50, 3.00)), "timeout")
+		yield(belt.create_timer(rand_range(0.50, 1.50)), "timeout")
 
 func astro_drop():
 	randomize()
 	var new_point = belt.choose(origin_points)
-	
 	var new_astro = astro_drop.instance()
+	
+	spawn_x = rand_range(200, 1400)
+	spawn_y = rand_range(200, 700)
 	
 	if new_point == "east":
 		new_astro.origin_pos = "east"
-		new_astro.position = Vector2(east_x, east_y)
+		new_astro.position = Vector2(east_x, spawn_y)
 	elif new_point == "west":
 		new_astro.origin_pos = "west"
-		new_astro.position = Vector2(west_x, west_y)
+		new_astro.position = Vector2(west_x, spawn_y)
+	elif new_point == "north":
+		new_astro.origin_pos = "north"
+		new_astro.position = Vector2(spawn_x, north_y)
+	elif new_point == "south":
+		new_astro.origin_pos = "south"
+		new_astro.position = Vector2(spawn_x, south_y)
 		
 	get_parent().add_child(new_astro)
