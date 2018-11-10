4cfed8ecee5686e8d20758c49f3afa9c5c768113
Güneş Eser <spielersun@gmail.com>
2018-11-10 22:35:11 +0300

    shooting potatoes, again

--- testerra/cartoffel.gd
+++ testerra/cartoffel.gd
@@ -1,11 +1,14 @@
 extends KinematicBody2D
 
+export(PackedScene) var bullet
+
 const UP = Vector2(0, -1)
 const GRAVITY = 20
 const ACCELERATION = 50
 const MAX_SPEED = 200
 const JUMP_HEIGHT = 500
 const SPEED = 100
+const BULLET_SPEED = 200
 
 var motion = Vector2()
 
@@ -15,6 +18,7 @@ var relative_width
 
 onready var sprites = $sprites
 onready var turret = $turret
+onready var beam = $beam
 
 var direction = 1
 
@@ -46,12 +50,12 @@ func _physics_process(delta):
 	motion.y += GRAVITY
 	var friction = false
 	
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("fp_forward"):
 		motion.x += ACCELERATION
 		motion.x = min(motion.x+ACCELERATION, MAX_SPEED)
 		$sprites.flip_h = false
 		$sprites.play("active")
-	elif Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("fp_backwards"):
 		motion.x -= ACCELERATION
 		motion.x = max(motion.x-ACCELERATION, -MAX_SPEED)
 		$sprites.flip_h = true
@@ -61,7 +65,7 @@ func _physics_process(delta):
 		friction = true
 	
 	if is_on_floor():
-		if Input.is_action_just_pressed("ui_up"):
+		if Input.is_action_just_pressed("fp_up"):
 			#print(motion.y)
 			motion.y = -JUMP_HEIGHT
 		if friction:
@@ -76,3 +80,23 @@ func _physics_process(delta):
 			motion.x = lerp(motion.x, 0, 0.05)
 		
 	motion = move_and_slide(motion, UP)
+	
+	if Input.is_action_just_pressed("fp_shoot"):
+		shoot(delta)
+
+func shoot(delta):
+	var new_bullet = bullet.instance()
+	var turret_angle = turret.rotation
+	# print(turret_angle)
+	
+	var bullet_x = (position.x + 5) + (27 * cos(turret_angle))
+	var bullet_y = (position.y + 12) + (27 * sin(turret_angle))
+	
+	beam.play()
+	
+	new_bullet.speed = BULLET_SPEED + SPEED
+	new_bullet.angle = turret_angle
+	new_bullet.position = Vector2(bullet_x, bullet_y) 
+	new_bullet.start_x = bullet_x
+	
+	get_parent().add_child(new_bullet)
