dde249a972b4a24ded29c476f13246c36c9af0f6
Güneş Eser <spielersun@gmail.com>
2018-11-12 22:58:02 +0300

    the pity of the moving mountains

--- testerra/menu.gd
+++ testerra/menu.gd
@@ -1,13 +1,14 @@
 extends Node
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+onready var camera = $camera
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
+
+func _physics_process(delta):
+	camera.position.x += delta * 100
 
 func _on_start_pressed():
 	get_tree().change_scene("res://testerra/episodeOne.tscn")
