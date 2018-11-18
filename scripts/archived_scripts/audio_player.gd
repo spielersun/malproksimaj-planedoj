eabc2545ac018d9addf14c8f86fc8a777d5f09a3
Güneş Eser <spielersun@gmail.com>
2018-11-18 17:42:24 +0300

    harsh past
    yeah not hard/

--- scripts/archived_scripts/audio_player.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-func _ready():
-	pass
-
-func play(sample_name):
-	## get_node(sample_name).play()
-	pass
--- scripts/archived_scripts/flash.gd
+++
@@ -1,8 +0,0 @@
-extends Sprite
-
-
-func _ready():
-	get_node("anim").play("fade_out")
-	yield(get_node("anim"), "animation_finished")
-	queue_free()
-
